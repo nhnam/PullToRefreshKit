a04b4c38241053f1d0c5d569ef976d8fddbe2b56
ナム Nam Nguyen <nam@ring.md>
2016-12-08 17:50:44 +0700

    Change to Framework

---
+++ PullToRefresh/PullToRefresh.h
@@ -0,0 +1,19 @@
+//
+//  PullToRefresh.h
+//  PullToRefresh
+//
+//  Created by ナム Nam Nguyen on 12/8/16.
+//  Copyright © 2016 Leo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PullToRefresh.
+FOUNDATION_EXPORT double PullToRefreshVersionNumber;
+
+//! Project version string for PullToRefresh.
+FOUNDATION_EXPORT const unsigned char PullToRefreshVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PullToRefresh/PublicHeader.h>
+
+
